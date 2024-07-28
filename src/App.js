4ea3bfd29161e3060431520dd58f82995c0c8891
mlohithraj <rlohith767@gmail.com>
2024-07-28 19:47:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import Hero from './sections/Hero';
+import CustomerReviews from './sections/CustomerReviews';
+import Footer from './sections/Footer';
+import PopularProducts from './sections/PopularProducts';
+import Services from './sections/Services';
+import SpecialOffer from './sections/SpecialOffer';
+import SuperQuality from './sections/SuperQuality';
+import Subscribe from './sections/Subscribe';
+import Nav from './components/Nav';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="relative">
+      <Nav />
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="padding-x py-10">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffer />
+      </section>
+      <section className="bg-pale-blue padding">
+        <CustomerReviews />
+      </section>
+      <section className="padding-x sm:py-32 py:16 w-full">
+        <Subscribe />
+      </section>
+      <section className="bg-black padding-x padding-t pb-8">
+        <Footer />
+      </section>
+    </main>
   );
 }
 
