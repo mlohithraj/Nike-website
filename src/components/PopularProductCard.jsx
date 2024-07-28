46edf52dad3da4de7678d37c5307a46ea42afdab
mlohithraj <rlohith767@gmail.com>
2024-07-28 19:59:42 +0530

    commit

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 import { star } from '../assets/icons';
 
 const PopularProductCard = ({ imgURL, name, price }) => {
+
+  const handleUnit = () => {
+    console.log(name, price)
+  }
+
   return (
-    <div className="flex flex-1 flex-col w-full max-sm:w-full">
+    <div className="flex flex-1 flex-col w-full max-sm:w-full" onClick={handleUnit}>
       <img src={imgURL} alt={name} className="w-[280px] h-[280px] " />
       <div className="mt-8 flex justify-start gap-2.5">
         <img src={star} alt="rating" width={24} height={24} />
